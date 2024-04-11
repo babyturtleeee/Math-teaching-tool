e1e616fbb14468e01c0ea79024b5c0e3988777f6
Xuze Peng <92495580+babyturtleeee@users.noreply.github.com>
2024-04-10 19:27:16 -0700

    Add files via upload

--- addon.py
+++ addon.py
@@ -3,13 +3,14 @@ from bpy.types import Operator
 import addon_utils
 
 # Install these
-import scipy
 import numpy as np
+
+import scipy
 from scipy.optimize import minimize
+from scipy.misc import derivative
 
 from sympy import diff, Symbol
 from sympy.parsing.sympy_parser import parse_expr
-from scipy.misc import derivative
 from sympy import sin
 from sympy import cos
 
@@ -44,28 +45,16 @@ def update_prop(self, value):
         variable_A = self.A
         variable_B = self.B
         variable_C = self.C
-        
         x_size = self.x
         y_size = self.y
-        
         x_start = self.start_x
         y_start = self.start_y
-        
-        global x_span
-        global y_span
-        global func_string
-        
         func_string = self.function
-        
-        x_span = x_size
-        y_span = y_size
         
         x_division = int(6.4 * x_size)
         y_division = int(6.4 * y_size)
         
         eqn = func_string.replace("A","{}").format(variable_A).replace("B","{}").format(variable_B).replace("C","{}").format(variable_C)
-        
-        print(eqn)
         
         func_string = eqn
         
@@ -85,10 +74,8 @@ def update_prop(self, value):
         mat = bpy.data.materials.get("Material_height_map")
         
         if new_surface.data.materials:
-            # assign to 1st material slot
             new_surface.data.materials[0] = mat
         else:
-            # no slots
             new_surface.data.materials.append(mat)
         
         new_surface.name = "Function_surface"
@@ -104,10 +91,8 @@ def update_prop(self, value):
         mat_frame = bpy.data.materials.get("Material_wire_frame")
             
         if new_surface_frame.data.materials:
-            # assign to 1st material slot
             new_surface_frame.data.materials[0] = mat_frame
         else:
-            # no slots
             new_surface_frame.data.materials.append(mat_frame)
             
         new_surface_frame.name = "Function_wire_frame"
@@ -137,10 +122,6 @@ def update_prop(self, value):
         C.scene.objects.get("z_axis").location.x = -(x_size/2.0) 
         C.scene.objects.get("z_axis").location.y =  (y_size/2.0) 
         C.scene.objects.get("z_axis").dimensions[2] = (variable_A*3.0) 
-        
-        #C.scene.objects.get("z_arrow_head").location.x = -(x_size/2.0)
-        #C.scene.objects.get("z_arrow_head").location.y =  (y_size/2.0)
-        #C.scene.objects.get("z_arrow_head").location.z =  (variable_A*3.0)
         
         C.scene.objects.get("z_axis_label").location.x = -(x_size/2.0) - 0.5
         C.scene.objects.get("z_axis_label").location.y =  (y_size/2.0) + 0.5
@@ -173,6 +154,7 @@ def update_prop(self, value):
         bpy.ops.mesh.primitive_uv_sphere_add(radius=0.1, enter_editmode=False, location=(0.0,0.0,0.0))
 
         sphere = context.object 
+        
         # making curve
         crv = bpy.data.curves.new('crv', 'CURVE')
         crv.dimensions = '3D'
@@ -212,7 +194,7 @@ def update_prop(self, value):
     else:
         print("Beginning dragging the slider !")
         self.is_dragging = True
-        bpy.ops.draggableprop.subscribe('INVOKE_DEFAULT')
+        bpy.ops.draggableprop.input('INVOKE_DEFAULT')
 
 def f(x, y, func): return eval(func)
 
@@ -264,6 +246,7 @@ class Maxmin_operator(Operator):
         return {'FINISHED'}
     
 class Extrusion_operator(Operator):
+    """ tooltip goes here """
     bl_idname = "object.output"
     bl_label = "Generate 3D Printing File"
 
@@ -317,17 +300,17 @@ class Extrusion_operator(Operator):
         
         return {'FINISHED'}
 
-class DRAGGABLEPROP_OT_subscribe(bpy.types.Operator):
-    bl_idname  = "draggableprop.subscribe"
+class DRAGGABLEPROP_input(bpy.types.Operator):
+    bl_idname  = "draggableprop.input"
     bl_label   = ""
-    stop: bpy.props.BoolProperty()  
+    stop: bpy.props.BoolProperty() 
 
     def modal(self, context, event):
         if self.stop:
             context.scene.my_prop.is_dragging = False
             print("End Dragging !")
             return {'FINISHED'}
-        if event.value == 'RELEASE':  
+        if event.value == 'RELEASE':
             self.stop = True
 
         return {'PASS_THROUGH'}
@@ -338,7 +321,7 @@ class DRAGGABLEPROP_OT_subscribe(bpy.types.Operator):
         return {'RUNNING_MODAL'}
 
 
-class TEST_PT_draggable_prop(bpy.types.Panel):
+class PT_draggable_prop(bpy.types.Panel):
     bl_label = "Project Tool Main Panel"
     bl_idname = "PT_MainPanel"
     bl_space_type = "VIEW_3D"
@@ -407,7 +390,14 @@ class TEST_PT_draggable_prop(bpy.types.Panel):
         row.prop(context.scene.my_prop, "text_scale")
         row = box.row()
         row.operator(Extrusion_operator.bl_idname,text = "Generate")
-        
+       
+def PanelRegistration():
+    bpy.utils.register_class(DRAGGABLEPROP_input)  
+    bpy.utils.register_class(PT_draggable_prop) 
+    bpy.utils.register_class(DraggableProp)  
+    bpy.utils.register_class(Extrusion_operator)  
+    bpy.utils.register_class(Maxmin_operator)  
+    bpy.types.Scene.my_prop = bpy.props.PointerProperty(type=DraggableProp)  
 
 class DraggableProp(bpy.types.PropertyGroup):
     A: bpy.props.FloatProperty(update=update_prop, min=-100, max=100)
@@ -431,15 +421,7 @@ class DraggableProp(bpy.types.PropertyGroup):
     text_scale: bpy.props.FloatProperty(name = "Text Scale", update=update_prop, min=-0, max=5,default = 1.5)
     
     is_dragging: bpy.props.BoolProperty()
-    
-def PanelRegistration():
-    bpy.utils.register_class(DRAGGABLEPROP_OT_subscribe)  # Register modal operator
-    bpy.utils.register_class(TEST_PT_draggable_prop)  # Register Panel to see what's going on
-    bpy.utils.register_class(DraggableProp)  # Register our custom prop
-    bpy.utils.register_class(Extrusion_operator)  # Register our custom prop
-    bpy.utils.register_class(Maxmin_operator)  # Register our custom prop
-    bpy.types.Scene.my_prop = bpy.props.PointerProperty(type=DraggableProp)  # Setup our custom prop
-    
+
 def main():
     purge_orphans()
     enable_addon(addon_module_name="add_mesh_extra_objects")
